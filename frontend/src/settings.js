0e4a1999db36f51585764a68c7215641e9674d5c
wm-ax <30f0fn@gmail.com>
2020-08-26 07:29:49 -0400

    fix settings.js

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,18 +1,2 @@
-import axios from 'axios';
-
-// const LOCAL_ROCKET = {
-//     API_URL : 'http://localhost:8000/api',
-//     axios_with_credentials : true,
-//     AXIOS : axios,
-// }
-
-export const API_ROOT_URL = process.env.WM_API_URL || `http://localhost:8000/api`;
-export const FRONTEND_ROOT_URL = process.env.WM_FRONTEND_URL || `http://localhost:3000`;
-
-// export const settings = LOCAL_ROCKET;
-
-// axios.withCredentials = settings['axios_with_credentials'];
-
-export const api = axios;
-
-// export default settings;
+export const FRONTEND_ROOT_URL = process.env.REACT_APP_FRONTEND_URL || `http://localhost:3000`;
+export const API_ROOT_URL = process.env.REACT_APP_API_URL || `http://localhost:8000/api`;
